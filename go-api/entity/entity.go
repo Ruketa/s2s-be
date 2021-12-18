01b7a00ebfa6aa17b15d1e17b86a96e2bffd6713
Ruketa <tkrkrhr@gmail.com>
2021-12-18 09:59:17 +0900

    Sessionに開催日を追加

--- go-api/entity/entity.go
+++ go-api/entity/entity.go
@@ -32,5 +32,6 @@ type StudySession struct {
 	Id          string    `json:"id"`
 	Holding_num int       `json:"holding_num" binding:"required"`
 	Plan_id     string    `json:"plan_id" binding:"required"`
+	Evented_on  time.Time `json:"evented_on" binding:"required"`
 	Created_at  time.Time `json:"created_at"`
 }
