696d03b44987464f7779b3ce2493b7e44be63adc
Ruketa <tkrkrhr@gmail.com>
2021-12-19 10:49:17 +0900

    bugfix of validation

--- go-api/entity/entity.go
+++ go-api/entity/entity.go
@@ -7,13 +7,13 @@ import (
 // Questionnaire entity
 type Questionnaire struct {
 	Id                   string    `json:"id"`
-	Satisfaction_level   int       `json:"satisfaction_level" binding:"required, min=0, max=5"`
-	Recommendation_level int       `json:"recommendation_level" binding:"required, min=0, max=5"`
-	Topics               string    `json:"topics" binding:"required"`
-	Participation_level  int       `json:"participation_level" binding:"required, min=0, max=5"`
-	Presentation_level   int       `json:"presentation_level" binding:"required, min=0, max=5"`
-	Free_comment         string    `json:"free_comment" binding:"required"`
-	Holding_num          int       `json:"holding_num" binding:"required, min=0"`
+	Satisfaction_level   int       `json:"satisfaction_level" validate:"required, gte=0, lte=5"`
+	Recommendation_level int       `json:"recommendation_level" validate:"required, gte=0, lte=5"`
+	Topics               string    `json:"topics" validate:"required"`
+	Participation_level  int       `json:"participation_level" validate:"required, gte=0, lte=5"`
+	Presentation_level   int       `json:"presentation_level" validate:"required, gte=0, lte=5"`
+	Free_comment         string    `json:"free_comment" validate:"required"`
+	Holding_num          int       `json:"holding_num" validate:"required, gte=0"`
 	Created_at           time.Time `json:"created_at"`
 }
 
